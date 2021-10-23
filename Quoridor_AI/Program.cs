718f37674031933f33215acd4b2b47cbe1b5205a
A-Danylevych <anton231255@gmail.com>
2021-10-23 15:00:53 +0300

    Initial commit

---
+++ Quoridor_AI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Quoridor_AI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
