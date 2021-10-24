97db5d436eac38b195a5b47d75dcd1c609d1766c
A-Danylevych <anton231255@gmail.com>
2021-10-24 20:44:33 +0300

    Finding optimal wall

--- Quoridor_AI/Model/Bot.cs
+++ Quoridor_AI/Model/Bot.cs
@@ -7,7 +7,8 @@ namespace Quoridor_AI.Model
     internal class Bot : Player
     {
         private List<Wall> _wallsSpots;
-        private int Top;
+        private int _winningTop;
+        private int _losingTop;
 
         public void MakeAMove(IController controller, Player otherPlayer)
         {
@@ -139,6 +140,39 @@ namespace Quoridor_AI.Model
                 return (bestPath, best);
             }
         }
+        
+        private Wall MinimaxWall(Cell otherPlayerPosition, int depth, 
+            int alpha, int beta, ICollection<Cell> visited, bool maximizingPlayer)
+        {
+            var (list, _) = Minimax(otherPlayerPosition, CurrentCell, depth, int.MinValue,
+                int.MaxValue, new List<Cell>(), true);
+
+            int leftCoord;
+            if (otherPlayerPosition.Coords.Top == list[0].Coords.Top)
+            {
+                var topCoord = otherPlayerPosition.Coords.Top;
+                if (otherPlayerPosition.Coords.Left == list[0].Coords.Left-75)
+                {
+                    leftCoord = otherPlayerPosition.Coords.Left + 50;
+                    return new Wall(new CellCoords(topCoord, leftCoord), true);
+                }
+
+                leftCoord = otherPlayerPosition.Coords.Left - 50;
+                return new Wall(new CellCoords(topCoord, leftCoord), true);
+            }
+
+            leftCoord = otherPlayerPosition.Coords.Left;
+            if (otherPlayerPosition.Coords.Top == list[0].Coords.Top - 75)
+            {
+                var topCoord = otherPlayerPosition.Coords.Left + 50;
+                return new Wall(new CellCoords(topCoord, leftCoord), false);
+            }
+            else
+            {
+                var topCoord = otherPlayerPosition.Coords.Left - 50;
+                return new Wall(new CellCoords(topCoord, leftCoord), false);
+            }
+        }
 
         private void WallSpots()
         {
@@ -180,9 +214,10 @@ namespace Quoridor_AI.Model
             } 
         }
 
-        public Bot(Color color, Cell cell, int top) : base(color, cell)
+        public Bot(Color color, Cell cell, int winningTop) : base(color, cell)
         {
-            Top = top;
+            _winningTop = winningTop;
+            _losingTop = cell.Coords.Top;
             WallSpots();
         }
     }
