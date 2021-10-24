a6576a4a59d84bc801c53b17134842a08854a21d
A-Danylevych <anton231255@gmail.com>
2021-10-24 13:16:48 +0300

    Minimax base

--- Quoridor_AI/Model/Bot.cs
+++ Quoridor_AI/Model/Bot.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Quoridor_AI.Model
 {
@@ -60,13 +61,81 @@ namespace Quoridor_AI.Model
             }
         }
 
-        private void Minimax(Cell playerPosition, Cell otherPlayerPosition, int depth, int alpha, int beta, 
-            List<Cell> visited, bool maximizingPlayer)
+        private (List<Cell> path, int score) Minimax(Cell playerPosition, Cell otherPlayerPosition, int depth, int alpha, int beta, 
+            ICollection<Cell> visited, bool maximizingPlayer)
         {
-            if (visited.Count == 81)
+            var unVisitedNeighbors = playerPosition.GetNeighbors().Except(visited);
+            if (!unVisitedNeighbors.Any() || depth == 0)
             {
-                
+                return (null, 0);
             }
+
+            if (maximizingPlayer)
+            {
+                var best = int.MinValue;
+                var bestPath = new List<Cell>(visited);
+                var placedToPath = false;
+                foreach (var neighbor in playerPosition.GetNeighbors().Except(visited))
+                {
+                    visited.Add(neighbor);
+                    var (_, score) = Minimax(playerPosition, otherPlayerPosition, depth - 1,
+                        alpha, beta, visited, false);
+                    if (score > best)
+                    {
+                        best = score;
+                        if (placedToPath)
+                        {
+                            bestPath.RemoveAt(bestPath.Count -1);
+                        }
+
+                        placedToPath = true;
+                            
+                        bestPath.Add(neighbor);
+                            
+                    }
+
+                    visited.Remove(neighbor);
+                    alpha = Math.Max(alpha, best);
+                    if(beta <= alpha)
+                    {
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                var best = int.MaxValue;
+                var bestPath = new List<Cell>(visited);
+                var placedToPath = false;
+                foreach (var neighbor in playerPosition.GetNeighbors().Except(visited))
+                {
+                    visited.Add(neighbor);
+                    var (_, score) = Minimax(playerPosition, otherPlayerPosition, depth - 1,
+                        alpha, beta, visited, false);
+                    if (score < best)
+                    {
+                        best = score;
+                        if (placedToPath)
+                        {
+                            bestPath.RemoveAt(bestPath.Count -1);
+                        }
+
+                        placedToPath = true;
+                            
+                        bestPath.Add(neighbor);
+                            
+                    }
+
+                    visited.Remove(neighbor);
+                    beta = Math.Min(beta, best);
+                    if(beta <= alpha)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            return (null, 0);
         }
 
         private void WallSpots()
