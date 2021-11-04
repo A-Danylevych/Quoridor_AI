a4ecacac207023a946feb907f2d8584d3ea8fec8
Mikhail <osovyk02@gmail.com>
2021-11-04 21:46:56 +0200

    make a move reFACtoring

--- Quoridor_AI/Model/Bot.cs
+++ Quoridor_AI/Model/Bot.cs
@@ -20,7 +20,7 @@ namespace Quoridor_AI.Model
             var (wall, wallScore) = MinimaxWall(this, otherPlayer, 3);
             
             var jumpingCells = MoveValidator.PossibleToMoveCells(this, otherPlayer, true);
-            if (jumpingCells.Count > 0)
+            if (jumpingCells.Count>0)
             {
                 foreach (var jump in jumpingCells.Where(jump => Math.Abs(jump.Coords.Top - _winningTop) 
                     - Math.Abs(CurrentCell.Coords.Top - _winningTop) < 0))
@@ -77,11 +77,13 @@ namespace Quoridor_AI.Model
                 return;
             }
 
+            const int startCoords = 25;
+            const int cellDistance = 75;
             var temp = CurrentCell;
             _board.MovePlayer(this, cell);
             if (MoveValidator.PossibleToMoveCells(otherPlayer, this, true).Count > 0 && 
-                Math.Abs(temp.Coords.Top - _winningTop - 25)/75 -
-                Math.Abs(otherPlayer.CurrentCell.Coords.Top - _losingTop - 25)/75 < 0)
+                Math.Abs(temp.Coords.Top - _winningTop - startCoords)/cellDistance -
+                Math.Abs(otherPlayer.CurrentCell.Coords.Top - _losingTop - startCoords)/cellDistance < 0)
             {
                 _board.MovePlayer(this, temp);
                 var list = new List<Cell>();
@@ -278,7 +280,7 @@ namespace Quoridor_AI.Model
                 if (otherPlayer.CurrentCell.Coords.Top == cell.Coords.Top)
                 {
                     var topCoord = otherPlayer.CurrentCell.Coords.Top;
-                    if (otherPlayer.CurrentCell.Coords.Left - cellDistance == cell.Coords.Left)
+                    if (otherPlayer.CurrentCell.Coords.Left -  cellDistance == cell.Coords.Left)
                     {
                         leftCoord = otherPlayer.CurrentCell.Coords.Left - leftUpWall;
                         return (new Wall(new CellCoords(topCoord, leftCoord), true), score);
